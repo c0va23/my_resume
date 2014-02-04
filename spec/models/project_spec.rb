5daf7bd9bc3d204ef3edd4bd9a42e27062ba721a
Dmitrij Fedorenko <c0va23@gmail.com>
2014-02-04 18:52:30 +0400

    Added model "Project".

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Project do
+  it { should have_db_column(:name).of_type(:string).with_options(null: false) }
+  it { should have_db_column(:description).of_type(:text) }
+  it { should have_db_column(:url).of_type(:string) }
+
+  it { should validate_presence_of(:name) }
+end
