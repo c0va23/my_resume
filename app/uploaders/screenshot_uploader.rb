81fab7fef9d71a12aa8a351cd4a5e4469392ed57
Dmitrij Fedorenko <c0va23@gmail.com>
2014-06-17 15:55:27 +0400

    Added generation of secure token for sceenshot.

--- app/uploaders/screenshot_uploader.rb
+++ app/uploaders/screenshot_uploader.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 class ScreenshotUploader < CarrierWave::Uploader::Base
+  TOKEN_LENGTH = 8
 
   include CarrierWave::MiniMagick
 
@@ -22,12 +23,33 @@ class ScreenshotUploader < CarrierWave::Uploader::Base
     process :resize_to_fit => [ 200, 200 ]
   end
 
+  before :cache, :generate_secure_token
+
   def extension_white_list
     %w(jpg jpeg gif png)
   end
 
-  def full_filename(filename)
-    (self.version_name.try(:to_s) || 'original') + File.extname(original_filename)
+  def filename
+    "#{secure_token}#{extension}"
+  end
+
+private
+
+  def generate_secure_token(file)
+    new_secure_token = SecureRandom.hex(TOKEN_LENGTH)
+    self.model.send("#{mounted_as}=", new_secure_token)
+  end
+
+  def secure_token
+    self.model.send(mounted_as)
+  end
+
+  def version_prefix
+    self.version_name.try(:to_s) || 'original'
+  end
+
+  def extension
+    File.extname(original_filename)
   end
 
 end
--- spec/uploaders/screenshoot_uploader_spec.rb
+++ spec/uploaders/screenshoot_uploader_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe ScreenshotUploader do
   let(:object) { OpenStruct.new(id: 1, screenshot: nil) }
+
   let(:screenshot_uploader) { ScreenshotUploader.new(object, :screenshot) }
 
   before { ScreenshotUploader.enable_processing = true }
@@ -12,18 +13,37 @@ describe ScreenshotUploader do
   context 'screenshot16-9.png' do
     let(:screenshot_path) { Rails.root.join('spec/stubs/screenshot16-9.png') }
 
+    describe :model do
+      subject { object }
+
+      its(:screenshot) { should be }
+    end
+
     describe 'original' do
       subject { screenshot_uploader }
 
       it { should have_dimensions(1920, 1080) }
-      its(:path) { should be_end_with("/original.png") }
+
+      describe "filename" do
+        subject { File.basename(screenshot_uploader.path) }
+
+        it { should be_start_with(object.screenshot) }
+        it { should be_end_with(".png") }
+      end
     end
 
-    describe 'thumb' do
+    describe :thumb200 do
       subject { screenshot_uploader.thumb200 }
 
       it { should be_no_larger_than(200, 200) }
-      its(:path) { should be_end_with("/thumb200.png") }
+
+      describe "filename" do
+        subject { File.basename(screenshot_uploader.thumb200.path) }
+
+        it { should be_start_with("thumb200") }
+        it { should be_include(object.screenshot) }
+        it { should be_end_with(".png") }
+      end
     end
   end
 
@@ -34,14 +54,27 @@ describe ScreenshotUploader do
       subject { screenshot_uploader }
 
       it { should have_dimensions(800, 600) }
-      its(:path) { should be_end_with("/original.jpg") }
+
+      describe "filename" do
+        subject { File.basename(screenshot_uploader.path) }
+
+        it { should be_start_with(object.screenshot) }
+        it { should be_end_with(".jpg") }
+      end
     end
 
-    describe 'thumb' do
+    describe :thumb200 do
       subject { screenshot_uploader.thumb200 }
 
       it { should be_no_larger_than(200, 200) }
-      its(:path) { should be_end_with("/thumb200.jpg") }
+
+      describe "filename" do
+        subject { File.basename(screenshot_uploader.thumb200.path) }
+
+        it { should be_start_with("thumb200") }
+        it { should be_include(object.screenshot) }
+        it { should be_end_with(".jpg") }
+      end
     end
   end
 
