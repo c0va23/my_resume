04d2c944db82d48d53fafd49bcd3d83287a902fe
Dmitrij Fedorenko <c0va23@gmail.com>
2014-06-19 14:11:09 +0400

    Reorganization of admin menus.

--- app/navigations/admin_navigation.rb
+++ app/navigations/admin_navigation.rb
@@ -8,31 +8,41 @@ SimpleNavigation::Configuration.run do |navigation|
     primary.dom_class = "nav navbar-nav"
     primary.selected_class = "active"
 
-    primary.item :projects, t('admin_navigation.projects'), admin_projects_path, highlights_on: :subpath do |project_navigation|
-      project_navigation.dom_class = "nav nav-pills"
-      project_navigation.selected_class = "active"
+    primary.item :projects, t('admin_navigation.projects'), admin_projects_path, highlights_on: :subpath do |projects_navigation|
+      projects_navigation.dom_class = "nav nav-tabs"
+      projects_navigation.selected_class = "active"
 
       if @project.try(:persisted?)
-        project_navigation.item :show_project, t('admin_navigation.show'), admin_project_path(@project)
-        project_navigation.item :edit_project, t('admin_navigation.edit'), edit_admin_project_path(@project)
-        project_navigation.item :delete_project, t('admin_navigation.delete'), delete_admin_project_path(@project)
+        projects_navigation.item :show_project, t('admin_navigation.show_project'), admin_project_path(@project)
+        projects_navigation.item :edit_project, t('admin_navigation.edit_project'), edit_admin_project_path(@project)
+        projects_navigation.item :delete_project, t('admin_navigation.delete_project'), delete_admin_project_path(@project)
 
-        project_navigation.item :screenshots, t('admin_navigation.screenshots'), admin_project_screenshots_path(@project)
-        project_navigation.item :new_screenshots, t('admin_navigation.new_screenshots'), new_admin_project_screenshot_path(@project)
+        projects_navigation.item :screenshots, t('admin_navigation.screenshots'), admin_project_screenshots_path(@project) do |screenshots_navigation|
+          screenshots_navigation.dom_class = "nav nav-tabs"
+          screenshots_navigation.selected_class = "active"
+
+          if @screenshot.try(:persisted?)
+            screenshots_navigation.item :show_screenshot, t('admin_navigation.show_screenshot'), admin_project_screenshot_path(@project, @screenshot)
+            screenshots_navigation.item :edit_screenshot, t('admin_navigation.edit_screenshot'), edit_admin_project_screenshot_path(@project, @screenshot)
+            screenshots_navigation.item :delete_screenshot, t('admin_navigation.delete_screenshot'), delete_admin_project_screenshot_path(@project, @screenshot)
+          else
+            screenshots_navigation.item :new_screenshot, t('admin_navigation.new_screenshot'), new_admin_project_screenshot_path(@project)
+          end
+        end
       else
-        project_navigation.item :new_object, t('admin_navigation.new'), new_admin_project_path
+        projects_navigation.item :new_project, t('admin_navigation.new_project'), new_admin_project_path
       end
     end
 
-    primary.item :tools, t('admin_navigation.tools'), admin_tools_path, highlights_on: :subpath do |tool_navigation|
-      tool_navigation.dom_class = "nav nav-pills"
-      tool_navigation.selected_class = "active"
+    primary.item :tools, t('admin_navigation.tools'), admin_tools_path, highlights_on: :subpath do |tools_navigation|
+      tools_navigation.dom_class = "nav nav-pills"
+      tools_navigation.selected_class = "active"
       if @tool.try(:persisted?)
-        tool_navigation.item :show_tool, t('admin_navigation.show'), admin_tool_path(@tool)
-        tool_navigation.item :edit_tool, t('admin_navigation.edit'), edit_admin_tool_path(@tool)
-        tool_navigation.item :delete_tool, t('admin_navigation.delete'), delete_admin_tool_path(@tool)
+        tools_navigation.item :show_tool, t('admin_navigation.show_tool'), admin_tool_path(@tool)
+        tools_navigation.item :edit_tool, t('admin_navigation.edit_tool'), edit_admin_tool_path(@tool)
+        tools_navigation.item :delete_tool, t('admin_navigation.delete_tool'), delete_admin_tool_path(@tool)
       else
-        tool_navigation.item :new_tool, t('admin_navigation.new'), new_admin_tool_path
+        tools_navigation.item :new_tool, t('admin_navigation.new_tool'), new_admin_tool_path
       end
     end
   end
