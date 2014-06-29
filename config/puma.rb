91f46b01e07f969b5b0620d80cc92e41d497a88a
Dmitrij Fedorenko <c0va23@gmail.com>
2014-06-29 15:21:59 +0400

    Configure puma.

---
+++ config/puma.rb
@@ -0,0 +1,16 @@
+PUMA_THREADS_MIN = Integer(ENV['PUMA_THREADS_MIN'] || 0)
+PUMA_THREADS_MAX = Integer(ENV['PUMA_THREADS_MAX'] || 1)
+PUMA_WORKERS = Integer(ENV['PUMA_WORKERS'] || 1)
+
+threads PUMA_THREADS_MIN, PUMA_THREADS_MAX
+workers PUMA_WORKERS if PUMA_WORKERS > 1
+
+preload_app!
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    config = ActiveRecord::Base.configurations[Rails.env]
+    config['pool'] = PUMA_THREADS_MAX
+    ActiveRecord::Base.establish_connection(config)
+  end
+end
