af92e94e0e82601ad8cd5c9390ea7ed35ec01250
Dmitrij Fedorenko <c0va23@gmail.com>
2014-07-01 19:35:48 +0400

    Configure loading translation files from sub-directories "config/locales/**".

--- config/application.rb
+++ config/application.rb
@@ -17,10 +17,10 @@ module MyResume
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.enforce_available_locales = true
     config.i18n.available_locales = [ :ru, :en ]
     config.i18n.default_locale = :ru
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml')]
 
     config.generators do |g|
       g.test_framework :rspec
