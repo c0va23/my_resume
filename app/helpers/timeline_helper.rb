fe9ec5c78c705bea9420d36c0b03066a02d42f2d
Dmitrij Fedorenko <c0va23@gmail.com>
2014-07-03 16:14:02 +0400

    Fixed timeline.

--- app/helpers/timeline_helper.rb
+++ app/helpers/timeline_helper.rb
@@ -1,7 +1,7 @@
 module TimelineHelper
 
   def timeline_widget(time_slots_path, filter = false)
-    render 'shared/timeline', filter: filter
+    render 'shared/timeline', filter: filter, time_slots_path: time_slots_path
   end
 
 end
