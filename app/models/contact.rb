8c9e6fac02dbcf385d969a326012b45fdbd6bafc
Dmitrij Fedorenko <c0va23@gmail.com>
2015-05-04 12:13:22 +0300

    Improved url-validation.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,4 @@
 class Contact < ActiveRecord::Base
   validates :label, presence: true
-  validates :url, presence: true, uniqueness: true, url: true
+  validates :url, presence: true, uniqueness: true, url: { scheme: %w[ http https mailto skype ] }
 end
--- app/validators/url_validator.rb
+++ app/validators/url_validator.rb
@@ -1,18 +1,11 @@
 class UrlValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute_name, value)
-    unless URI.regexp(SUPPORTED_SCHEMAS) === value
+    schemes = [ *self.options[:scheme] ]
+    unless URI.regexp(schemes) === value
       # i18n-tasks-use t("errors.messages.invalid_uri")
       record.errors.add(attribute_name, :invalid_uri)
     end
   end
-
-private
-
-  SUPPORTED_SCHEMAS = %w[
-    http
-    https
-    mailto
-  ]
 
 end
