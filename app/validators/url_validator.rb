93932e8d95bc63776d8fb47676b22e77db78a74e
Dmitrij Fedorenko <c0va23@gmail.com>
2016-05-22 17:56:54 +0300

    Fix rubocop warnings for validators

--- app/validators/url_validator.rb
+++ app/validators/url_validator.rb
@@ -1,11 +1,8 @@
 class UrlValidator < ActiveModel::EachValidator
-
   def validate_each(record, attribute_name, value)
-    schemes = [ *self.options[:scheme] ]
-    unless URI.regexp(schemes) === value
-      # i18n-tasks-use t("errors.messages.invalid_uri")
-      record.errors.add(attribute_name, :invalid_uri)
-    end
+    schemes = [*options[:scheme]]
+    return if URI.regexp(schemes) === value
+    # i18n-tasks-use t("errors.messages.invalid_uri")
+    record.errors.add(attribute_name, :invalid_uri)
   end
-
 end
