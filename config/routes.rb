2c0041797010a0f02f06ee404842765f8f9a1f0c
Dmitrij Fedorenko <c0va23@gmail.com>
2016-05-22 14:19:38 +0300

    Fix rubocop warnings for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,31 @@
 MyResume::Application.routes.draw do
-
   root to: 'main#index'
 
-  resources :projects, only: %w[ index show ] do
+  resources :projects, only: %i( index show ) do
     resources :time_slots, only: :index
   end
-  resources :tools, only: %w[ index show ] do
+  resources :tools, only: %i( index show ) do
     resources :time_slots, only: :index
     member do
       get :versions
     end
   end
   resources :time_slots, only: :index
-  resources :pages, only: %w[ index show ]
+  resources :pages, only: %w( index show )
 
   get '/admin' => redirect('/admin/projects')
   namespace :admin do
     concern :deletable do
       get :delete, on: :member
     end
-    resources :projects, concerns: [ :deletable ] do
-      resources :screenshots, concerns: [ :deletable ]
-      resources :tool_projects, concerns: [ :deletable ]
-      resources :time_slots, concerns: [ :deletable ]
+    resources :projects, concerns: %i( deletable ) do
+      resources :screenshots, concerns: %i( deletable )
+      resources :tool_projects, concerns: %i( deletable )
+      resources :time_slots, concerns: %i( deletable )
     end
-    resources :tools, concerns: [ :deletable ]
-    resources :pages, concerns: [ :deletable ]
-    resources :tool_types, concerns: [ :deletable ]
-    resources :contacts, concerns: [ :deletable ]
+    resources :tools, concerns: %i( deletable )
+    resources :pages, concerns: %i( deletable )
+    resources :tool_types, concerns: %i( deletable )
+    resources :contacts, concerns: %i( deletable )
   end
-
 end
