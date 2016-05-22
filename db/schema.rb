02a7df02807393f173e66c1875c24af7121d34a5
Dmitrij Fedorenko <c0va23@gmail.com>
2016-05-22 14:20:59 +0300

    Fix rubocop warnings for schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,77 +11,75 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150503190122) do
-
+ActiveRecord::Schema.define(version: 20_150_503_190_122) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "contacts", force: :cascade do |t|
-    t.string   "label",      null: false
-    t.string   "url",        null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "value"
+  create_table 'contacts', force: :cascade do |t|
+    t.string 'label', null: false
+    t.string 'url', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'value'
   end
 
-  add_index "contacts", ["url"], name: "index_contacts_on_url", unique: true, using: :btree
+  add_index 'contacts', ['url'], name: 'index_contacts_on_url', unique: true, using: :btree
 
-  create_table "pages", force: :cascade do |t|
-    t.string   "title",      limit: 255
-    t.text     "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table 'pages', force: :cascade do |t|
+    t.string 'title', limit: 255
+    t.text 'content'
+    t.datetime 'created_at'
+    t.datetime 'updated_at'
   end
 
-  create_table "projects", force: :cascade do |t|
-    t.string   "name",        limit: 255, null: false
-    t.text     "description"
-    t.string   "url",         limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.text     "role"
+  create_table 'projects', force: :cascade do |t|
+    t.string 'name', limit: 255, null: false
+    t.text 'description'
+    t.string 'url', limit: 255
+    t.datetime 'created_at'
+    t.datetime 'updated_at'
+    t.text 'role'
   end
 
-  create_table "screenshots", force: :cascade do |t|
-    t.string   "name",        limit: 255
-    t.text     "description"
-    t.string   "image",       limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "project_id"
+  create_table 'screenshots', force: :cascade do |t|
+    t.string 'name', limit: 255
+    t.text 'description'
+    t.string 'image', limit: 255
+    t.datetime 'created_at'
+    t.datetime 'updated_at'
+    t.integer 'project_id'
   end
 
-  create_table "time_slots", force: :cascade do |t|
-    t.date     "started_at"
-    t.date     "ended_at"
-    t.integer  "project_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table 'time_slots', force: :cascade do |t|
+    t.date 'started_at'
+    t.date 'ended_at'
+    t.integer 'project_id'
+    t.datetime 'created_at'
+    t.datetime 'updated_at'
   end
 
-  add_index "time_slots", ["project_id"], name: "index_time_slots_on_project_id", using: :btree
+  add_index 'time_slots', ['project_id'], name: 'index_time_slots_on_project_id', using: :btree
 
-  create_table "tool_projects", force: :cascade do |t|
-    t.integer  "project_id"
-    t.integer  "tool_id"
-    t.string   "version",    limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table 'tool_projects', force: :cascade do |t|
+    t.integer 'project_id'
+    t.integer 'tool_id'
+    t.string 'version', limit: 255
+    t.datetime 'created_at'
+    t.datetime 'updated_at'
   end
 
-  create_table "tool_types", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table 'tool_types', force: :cascade do |t|
+    t.string 'name', limit: 255
+    t.datetime 'created_at'
+    t.datetime 'updated_at'
   end
 
-  create_table "tools", force: :cascade do |t|
-    t.string   "name",         limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "tool_type_id"
+  create_table 'tools', force: :cascade do |t|
+    t.string 'name', limit: 255
+    t.datetime 'created_at'
+    t.datetime 'updated_at'
+    t.integer 'tool_type_id'
   end
 
-  add_index "tools", ["tool_type_id"], name: "index_tools_on_tool_type_id", using: :btree
-
+  add_index 'tools', ['tool_type_id'], name: 'index_tools_on_tool_type_id', using: :btree
 end
