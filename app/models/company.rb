b349cca7c3014fe60f6de52c38bbdf6e09d73795
Dmitrij Fedorenko <c0va23@gmail.com>
2016-05-28 20:59:10 +0300

    Advance validation for company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,6 @@
 class Company < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
-  validates :started_at, presence: true
+  validates :started_at, presence: true, date: { before: proc { Time.current } }
+  validates :ended_at, date: { after: :started_at }
+  validates :site_url, url: { scheme: %w(http https) }
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -20,7 +20,31 @@ describe Company do
     it { should validate_uniqueness_of(:name) }
 
     it { should validate_presence_of(:started_at) }
+    it 'should not allow value of the future' do
+      should_not allow_value(1.day.since).for(:started_at)
+    end
 
-    it 'shoulda validate site_url'
+    it 'ended_at should be great than ended_at' do
+      should_not allow_value(subject.started_at - 1.day).for(:ended_at)
+    end
+
+    let(:valid_http_url) do
+      FFaker::Internet.uri(%i(http https).sample) + "/#{FFaker::Internet.slug}"
+    end
+    let(:valid_not_http_url) do
+      FFaker::Internet.uri(%i(ftp phone).sample) + "/#{FFaker::Internet.slug}"
+    end
+
+    it 'shoulda allow valid http url for site_url' do
+      should allow_value(valid_http_url).for(:site_url)
+    end
+
+    it 'shoulda not allow valid hot http url for site_url' do
+      should_not allow_value(valid_not_http_url).for(:site_url)
+    end
+
+    it 'should_not allow invalid url for site_url' do
+      should_not allow_value(FFaker::Internet.slug).for(:site_url)
+    end
   end
 end
