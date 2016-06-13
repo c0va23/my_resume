976140511a3541fd7b3327dcc0f5f99194ed8cec
Dmitrij Fedorenko <c0va23@gmail.com>
2016-06-13 14:31:44 +0300

    Shot 404 page for record not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
   def not_found
     render 'errors/not_found', status: :not_found
   end
