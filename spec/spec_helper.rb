4467143eb3807bd5158eeb232a87192f7e963b6a
Dmitrij Fedorenko <job@fedorenko-d.ru>
2019-03-28 09:34:55 +0300

    Fix run spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'rspec/rails'
 require 'shoulda/matchers'
 
 require 'bundler'
-Bundler.setup(:default, :test)
+Bundler.require(:default, :test)
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
