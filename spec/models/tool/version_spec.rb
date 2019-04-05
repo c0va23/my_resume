81f7ca45b25a8856906b5d3253f2d0700c36fd70
Dmitrij Fedorenko <job@fedorenko-d.ru>
2019-04-05 09:24:52 +0300

    Add specs for Tool::Version

--- spec/models/tool/version_spec.rb
+++ spec/models/tool/version_spec.rb
@@ -1,4 +1,18 @@
 require 'spec_helper'
 
 describe Tool::Version do
+  describe '#color' do
+    let(:random_version) { -> { "#{rand(10)}.#{rand(10)}.#{rand(10)}" } }
+
+    let(:version1) { described_class.new(random_version.call, 1.week) }
+    let(:version2) { described_class.new(random_version.call, 2.weeks) }
+
+    it 'return color in CSS format' do
+      expect(version1.color).to match(/\A#[0-9a-f]{6}\z/)
+    end
+
+    it 'return different value for differen version' do
+      expect(version1.color).not_to eq version2.color
+    end
+  end
 end
